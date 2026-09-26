291f77dc96c23f002e66ee373a6d9da4a88d86f1
Arnau <armarquesabello@gmail.com>
2026-09-26 14:10:16 +0200

    cambio a mysql remote de hostinger

--- backend/db_connection.js
+++ backend/db_connection.js
@@ -16,6 +16,9 @@ const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
 
 export const connection = mysql.createPool(config);
